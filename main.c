da17f5e5de8e3783e3d575d2d599098667cc724e
MaeinesenieaM <david.costa.gomes71@gmail.com>
2024-04-20 11:37:07 -0300

    Tranformando a separação e uma função separada.

--- main.c
+++ main.c
@@ -2,17 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define TAMANHO_MIN 32
+#define TAMANHO_MAX 64
+
 typedef struct {
-	char nome[16];
-	char telefone[16];
-	char curso[16];
+	char nome[TAMANHO_MIN];
+	char telefone[TAMANHO_MIN];
+	char curso[TAMANHO_MIN];
 	float nota1, nota2;
 } Aluno;
 
-//Função para organizar as informações de cada linha para uma struct chamada 'Aluno'.
-Aluno aluno_organiza (char frases[][16]) {
-	Aluno aluno;
 
+//Função para organizar as informações de cada linha para uma struct chamada 'Aluno'.
+Aluno aluno_organiza (char frases[][TAMANHO_MIN]) {
+	Aluno aluno;
 	strcpy (aluno.nome, frases[0]);
 	strcpy (aluno.telefone, frases[1]);
 	strcpy (aluno.curso, frases[2]);
@@ -24,34 +27,39 @@ Aluno aluno_organiza (char frases[][16]) {
 	return aluno;
 }
 
+void separador (char linha[], char frases[][TAMANHO_MIN]) {
+	char *memoria = linha;
+
+	for (int i = 0; i < 5; i++) {
+			//'strok_r()' usa um ponteiro da linha e quando chega em uma ',' retorna as informações anterior a ela.
+			strcpy(frases[i], strtok_r(memoria, ",", &memoria));
+	}
+}
+
 int main () {
 
-	FILE *arquivo;
-	arquivo = fopen("DadosEntrada.csv", "r");
+	FILE *entrada;
+	entrada = fopen("DadosEntrada.csv", "r");
 
-	if (arquivo == NULL) {
+	if (entrada == NULL) {
 		printf("Arquivo nao encontrado.");
 		exit(1);
 	}
 
-	char linha[64];
-	char corte[5][16];
+	char linha[TAMANHO_MAX];
+	char frases[5][TAMANHO_MIN];
 
-	//Este loop é temporario, ele primeiro ler a primeira linha do arquivo, e enquanto ela não for nula ira continuar repetindo.
-	while (fgets(linha, sizeof(linha), arquivo) != NULL) { //PARA FAZER! TRANSFORMAR ESTE LOOP INTEIRO EM UMA FUNÇÃO!
+	//Este loop ira ler as linhas do arquivo uma de cada vez.
+	while (fgets(linha, sizeof(linha), entrada) != NULL) {
 		strtok(linha, "\n"); //remove o fim da linha.
+		separador (linha, frases);
 
-		char *memoria = linha;
-		for (int i = 0; i < 5; i++) {
-			//'strok_r()' usa um ponteiro da linha e quando chega em uma ',' retorna as informações anterior a ela.
-			strcpy(corte[i], strtok_r(memoria, ",", &memoria)); 
-		}
-
-		Aluno aluno = aluno_organiza (corte);
+		Aluno aluno = aluno_organiza (frases);
+		//'printf' temporario.
 		printf ("[Aluno: %s | Nota: %.2f | Nota2: %.2f]\n", aluno.nome, aluno.nota1, aluno.nota2);
 	}
 
-	fclose (arquivo);
+	fclose (entrada);
 
 	return 0;
 }
