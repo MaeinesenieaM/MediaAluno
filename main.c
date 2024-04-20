ddaeaf5a4a6f6620e1879dc2b3e801a151e59e92
MaeinesenieaM <david.costa.gomes71@gmail.com>
2024-04-19 23:25:44 -0300

    Leitura do arquivo semi-completa, Limpeza de codigo desnecessario e Inclusão de comentario.

--- main.c
+++ main.c
@@ -1,43 +1,57 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 typedef struct {
-	char nome[64];
-	char telefone[15];
-	char curso[64];
+	char nome[16];
+	char telefone[16];
+	char curso[16];
 	float nota1, nota2;
 } Aluno;
 
-void organiza (char frases[]) {
+//Função para organizar as informações de cada linha para uma struct chamada 'Aluno'.
+Aluno aluno_organiza (char frases[][16]) {
+	Aluno aluno;
 
+	strcpy (aluno.nome, frases[0]);
+	strcpy (aluno.telefone, frases[1]);
+	strcpy (aluno.curso, frases[2]);
+
+	//'strtof()' converte strings em valores tipo float.
+	aluno.nota1 = strtof(frases[3], NULL);
+	aluno.nota2 = strtof(frases[4], NULL);
+	
+	return aluno;
 }
 
 int main () {
 
 	FILE *arquivo;
-
 	arquivo = fopen("DadosEntrada.csv", "r");
 
-	Aluno aluno;
-
 	if (arquivo == NULL) {
-		printf("Arquivo nao encontrado!");
+		printf("Arquivo nao encontrado.");
+		exit(1);
 	}
 
-	int debug = 0;
 	char linha[64];
 	char corte[5][16];
 
-	while (fgets(linha, sizeof(linha), arquivo) != NULL) {
-		strtok(linha, "\n");
-		char *save = linha;
+	//Este loop é temporario, ele primeiro ler a primeira linha do arquivo, e enquanto ela não for nula ira continuar repetindo.
+	while (fgets(linha, sizeof(linha), arquivo) != NULL) { //PARA FAZER! TRANSFORMAR ESTE LOOP INTEIRO EM UMA FUNÇÃO!
+		strtok(linha, "\n"); //remove o fim da linha.
+
+		char *memoria = linha;
 		for (int i = 0; i < 5; i++) {
-			strcpy(corte[i], strtok_r(save, ",", &save));
-			printf ("[%s]", corte[i]);
+			//'strok_r()' usa um ponteiro da linha e quando chega em uma ',' retorna as informações anterior a ela.
+			strcpy(corte[i], strtok_r(memoria, ",", &memoria)); 
 		}
+
+		Aluno aluno = aluno_organiza (corte);
+		printf ("[Aluno: %s | Nota: %.2f | Nota2: %.2f]\n", aluno.nome, aluno.nota1, aluno.nota2);
 	}
 
-	printf ("\nFUNCIONA!\n");
+	fclose (arquivo);
 
 	return 0;
 }
